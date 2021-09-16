0e9f76c79050fcca97fb83f1e827e080cd9346e7
Drew <andrewcaldwell@ac.c4q.nyc>
2021-09-16 11:41:45 -0400

    created component folder with sections of the webpage

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import './header.css';
+import factcheck from '../Images/factCheckImage.jpg'
+
+export default class Header extends Component{
+    render(){
+        return(
+            <div className="header">
+                <h1 className="header-title">Fact Check the FactChecker</h1>
+                <img className="image" alt="headerImage" src={factcheck} />
+            </div>
+        )
+    }
+}
