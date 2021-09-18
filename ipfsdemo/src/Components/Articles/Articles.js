8af0c83317a8ab4d8d9cd8385fce9ca5601b45da
Drew <andrewcaldwell@ac.c4q.nyc>
2021-09-18 19:24:11 -0400

    Updated Articles

--- ipfsdemo/src/Components/Articles/Articles.js
+++ ipfsdemo/src/Components/Articles/Articles.js
@@ -1,7 +1,9 @@
 import React, {Component} from 'react';
 import './articles.css';
 import Card from 'react-bootstrap/Card';
+import CardGroup from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
+import { Link } from 'react-router-dom';
 
 export default class Articles extends Component{
     render(){
@@ -19,7 +21,84 @@ export default class Articles extends Component{
                                         Some quick example text to build on the card title and make up the bulk of
                                         the card's content.
                                     </Card.Text>
-                                    <Button variant="primary">Read More</Button>
+                                    <Link to="/about">
+                                        <Button variant="primary">Read More</Button>
+                                    </Link>
+                                </Card.Body>
+                            </Card>
+                        </li>
+                        <li>
+                            <Card style={{ width: '18rem' }}>
+                                <Card.Img variant="top" src="holder.js/100px180" />
+                                <Card.Body>
+                                    <Card.Title>Card Title</Card.Title>
+                                    <Card.Text>
+                                        Some quick example text to build on the card title and make up the bulk of
+                                        the card's content.
+                                    </Card.Text>
+                                    <Link to="/about">
+                                        <Button variant="primary">Read More</Button>
+                                    </Link>
+                                </Card.Body>
+                            </Card>
+                        </li>
+                        <li>
+                            <Card style={{ width: '18rem' }}>
+                                <Card.Img variant="top" src="holder.js/100px180" />
+                                <Card.Body>
+                                    <Card.Title>Card Title</Card.Title>
+                                    <Card.Text>
+                                        Some quick example text to build on the card title and make up the bulk of
+                                        the card's content.
+                                    </Card.Text>
+                                    <Link to="/about">
+                                        <Button variant="primary">Read More</Button>
+                                    </Link>
+                                </Card.Body>
+                            </Card>
+                        </li>
+                        <li>
+                            <Card style={{ width: '18rem' }}>
+                                <Card.Img variant="top" src="holder.js/100px180" />
+                                <Card.Body>
+                                    <Card.Title>Card Title</Card.Title>
+                                    <Card.Text>
+                                        Some quick example text to build on the card title and make up the bulk of
+                                        the card's content.
+                                    </Card.Text>
+                                    <Link to="/about">
+                                        <Button variant="primary">Read More</Button>
+                                    </Link>
+                                </Card.Body>
+                            </Card>
+                        </li>
+                        <li>
+                            <Card style={{ width: '18rem' }}>
+                                <Card.Img variant="top" src="holder.js/100px180" />
+                                <Card.Body>
+                                    <Card.Title>Card Title</Card.Title>
+                                    <Card.Text>
+                                        Some quick example text to build on the card title and make up the bulk of
+                                        the card's content.
+                                    </Card.Text>
+                                    <Link to="/about">
+                                        <Button variant="primary">Read More</Button>
+                                    </Link>
+                                </Card.Body>
+                            </Card>
+                        </li>
+                        <li>
+                            <Card style={{ width: '18rem' }}>
+                                <Card.Img variant="top" src="holder.js/100px180" />
+                                <Card.Body>
+                                    <Card.Title>Card Title</Card.Title>
+                                    <Card.Text>
+                                        Some quick example text to build on the card title and make up the bulk of
+                                        the card's content.
+                                    </Card.Text>
+                                    <Link to="/about">
+                                        <Button variant="primary">Read More</Button>
+                                    </Link>
                                 </Card.Body>
                             </Card>
                         </li>
